826a9f4a61faf13da1b022d5e4c10f82a4cab016
krushna rathod <krushna.rathod@simformsolutions.com>
2025-03-28 16:30:53 +0530

    feature: create ProductDetails class for handle and print product details data

---
+++ src/scripts/view/productDetails.ts
@@ -0,0 +1,32 @@
+import { ApiServices } from '../controller/apiServices';
+import { templates } from './template';
+
+export class ProductDetails {
+    private apiServices: ApiServices;
+    private urlParams: URLSearchParams;
+    private productId: string;
+    private productDetailContainer: HTMLElement;
+
+    constructor() {
+        this.apiServices = new ApiServices();
+        this.urlParams = new URLSearchParams(window.location.search);
+        this.productId = this.urlParams.get('id')!;
+        this.productDetailContainer = document.getElementById(
+            'product-details'
+        ) as HTMLElement;
+        this.searchProductByID();
+    }
+
+    async searchProductByID() {
+        try {
+            const product = await this.apiServices.searchOneProduct(
+                this.productId
+            );
+
+            this.productDetailContainer.innerHTML =
+                templates.productDetails(product);
+        } catch (err) {
+            console.log('Error fetching product details:', err);
+        }
+    }
+}
