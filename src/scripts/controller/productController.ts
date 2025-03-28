1604b6e89bfb3b843b75af1522e5d554b4d6291a
krushna rathod <krushna.rathod@simformsolutions.com>
2025-03-28 18:59:53 +0530

    feature: add event handler foe searchbar

--- src/scripts/controller/productController.ts
+++ src/scripts/controller/productController.ts
@@ -1,4 +1,5 @@
 import { Product } from '../model/interfaces';
+import { debounce } from '../utils/debounce';
 import { ProductView } from '../view/productView';
 import { ApiServices } from './apiServices';
 
@@ -8,9 +9,16 @@ export class ProductControler {
     private isLoading: boolean = true;
     private view!: ProductView;
 
+    private searchBar: HTMLInputElement;
+
     constructor() {
         this.apiServices = new ApiServices();
         this.view = new ProductView();
+
+        this.searchBar = document.getElementById(
+            'search-bar'
+        ) as HTMLInputElement;
+
         this.initalize();
     }
 
@@ -33,9 +41,31 @@ export class ProductControler {
         }
     }
 
+    async searchData(query: string): Promise<void> {
+        try {
+            const apiProducts = await this.apiServices.searchProducts(query);
+            this.allProducts = [...apiProducts];
+            this.view.renderProducts(this.allProducts);
+        } catch (err) {
+            console.error('Search error:', err);
+        }
+    }
+
+    initSearchProduct(): void {
+        const debounceSearch = debounce((query: string) => {
+            this.searchData(query);
+        }, 500);
+
+        this.searchBar.addEventListener('input', (e) => {
+            const query = (e.target as HTMLInputElement).value;
+            debounceSearch(query);
+        });
+    }
+
     private eventHandler(): void {
         try {
             this.view.handleViewDetailsButton(this.allProducts);
+            this.initSearchProduct();
         } catch (err) {
             alert(`Error while getting product details ${err}`);
         }
