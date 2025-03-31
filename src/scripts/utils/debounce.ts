0c5a0eeb6fd1c2cb53cce045dc1155266754bbab
krushna rathod <krushna.rathod@simformsolutions.com>
2025-03-31 19:17:18 +0530

    feature: reconfigure debounce function

--- src/scripts/utils/debounce.ts
+++ src/scripts/utils/debounce.ts
@@ -2,11 +2,11 @@ export function debounce<T extends (...args: Parameters<T>) => void>(
     func: T,
     delay: number
 ) {
-    let timer: ReturnType<typeof setTimeout> | null = null;
-    return (...args: Parameters<T>) => {
-        if (timer) clearTimeout(timer);
+    let timer: ReturnType<typeof setTimeout> | undefined;
+    return function (this: unknown, ...args: Parameters<T>) {
+        if (timer !== undefined) clearTimeout(timer);
         timer = setTimeout(() => {
-            func(...args);
+            func.apply(this, args);
         }, delay);
     };
 }
