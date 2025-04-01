eb3565abb8ef1676da6466d2b07d315e3e67ad36
krushna rathod <krushna.rathod@simformsolutions.com>
2025-04-01 11:10:05 +0530

    feature: add functionality for add product and display product

--- src/scripts/controller/productController.ts
+++ src/scripts/controller/productController.ts
@@ -1,5 +1,7 @@
 import type { Product } from '../model/interfaces';
+import { addUpdateForm } from '../utils/addUpdateForm';
 import { debounce } from '../utils/debounce';
+import { StorageService } from '../utils/localStorageService';
 import { ProductView } from '../view/productView';
 import { ApiServices } from './apiServices';
 
@@ -10,6 +12,11 @@ export class ProductController {
     private view: ProductView;
 
     private searchBar: HTMLInputElement;
+    private addProductButton: HTMLButtonElement;
+    private addFormContainer: HTMLElement;
+    private closeFormButton: HTMLButtonElement;
+    private addProductForm: HTMLFormElement;
+    private editProductId: number | null = null;
 
     constructor() {
         this.apiServices = new ApiServices();
@@ -19,21 +26,38 @@ export class ProductController {
             'search-bar'
         ) as HTMLInputElement;
 
+        this.addProductButton = document.getElementById(
+            'add-button'
+        ) as HTMLButtonElement;
+
+        this.addFormContainer = document.getElementById(
+            'product-form-container'
+        ) as HTMLElement;
+
+        this.closeFormButton = document.getElementById(
+            'close-form-btn'
+        ) as HTMLButtonElement;
+
+        this.addProductForm = document.getElementById(
+            'product-form'
+        ) as HTMLFormElement;
+
         this.initialize();
     }
 
     private async initialize(): Promise<void> {
         this.view.loading();
         await this.fetchData();
+        this.attachEventHandlers();
     }
 
     async fetchData(): Promise<void> {
         try {
+            const localProducts: Array<Product> = StorageService.getProducts();
             const apiProducts = await this.apiServices.fetchProducts();
 
-            this.allProducts = [...apiProducts];
+            this.allProducts = [...localProducts, ...apiProducts];
             this.view.renderProducts(this.allProducts);
-            this.attachEventHandlers();
         } catch (err) {
             alert(`Get Error while loading product data ${err}`);
             console.error(`Get Error while loading product data ${err}`);
@@ -67,10 +91,106 @@ export class ProductController {
         });
     }
 
+    attachFormHandler(): void {
+        if (
+            !this.addProductButton ||
+            !this.addProductForm ||
+            !this.addFormContainer
+        ) {
+            console.error('elements are missing');
+            return;
+        }
+
+        this.addProductButton.addEventListener('click', () => {
+            this.editProductId = null;
+            this.addProductForm.reset();
+            this.addFormContainer.classList.remove('hidden');
+        });
+
+        this.closeFormButton.addEventListener('click', () => {
+            this.addFormContainer.classList.add('hidden');
+        });
+
+        this.addProductForm.removeEventListener('submit', this.onFormSubmit);
+        this.addProductForm.addEventListener('submit', this.onFormSubmit);
+    }
+
+    private onFormSubmit = async (e: Event) => {
+        e.preventDefault();
+        if (this.editProductId !== null) {
+            await this.updateExistingProduct();
+        } else {
+            await this.addNewProduct();
+        }
+    };
+
+    async addProduct(isUpdate: boolean = false) {
+        const getValue = (selector: string) =>
+            (
+                this.addProductForm.querySelector(selector) as HTMLInputElement
+            )?.value.trim() || '';
+
+        const title = getValue('#title');
+        const description = getValue('#description');
+        const category = getValue('#category');
+        const price = Number(getValue('#price'));
+        const stock = Number(getValue('#stock'));
+        const imageUrl = getValue('#imageUrl');
+
+        if (
+            !title ||
+            !description ||
+            !category ||
+            isNaN(price) ||
+            price <= 0 ||
+            isNaN(stock) ||
+            stock <= 0 ||
+            !imageUrl
+        ) {
+            alert('Please fill out all required fields with valid data.');
+            return;
+        }
+
+        const id = isUpdate
+            ? this.editProductId!
+            : Math.floor(Math.random() * 100000);
+        const productData = addUpdateForm.addUpdateProductService(
+            id,
+            title,
+            description,
+            category,
+            price,
+            stock,
+            imageUrl
+        );
+
+        if (isUpdate) {
+            StorageService.updateProduct(productData);
+            alert('Product updated successfully!');
+        } else {
+            StorageService.addProduct(productData);
+            alert('Product added successfully!');
+        }
+
+        this.addProductForm.reset();
+        this.addFormContainer.classList.add('hidden');
+        this.initialize();
+    }
+
+    async addNewProduct() {
+        await this.addProduct(false);
+    }
+
+    async updateExistingProduct() {
+        if (this.editProductId === null) return;
+        await this.addProduct(true);
+    }
+
     private attachEventHandlers(): void {
         try {
             this.view.handleViewDetailsButton(this.allProducts);
             this.attachSearchHandler();
+            this.attachFormHandler();
         } catch (err) {
             alert(`Error while getting product details ${err}`);
             console.error(`Error while getting product details ${err}`);
